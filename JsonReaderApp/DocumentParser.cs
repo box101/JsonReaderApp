752927c160e90f81aac64263c3cc3da19f6d4335
Антон Акулинин <akulinin@bars-open.ru>
2019-05-01 19:36:50 +0300

    Вторая часть марлезонского балета

---
+++ JsonReaderApp/DocumentParser.cs
@@ -0,0 +1,99 @@
+﻿namespace JsonReaderApp 
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    using Newtonsoft.Json.Linq;
+
+    /// <summary>
+    /// Json-document parser
+    /// </summary>
+    public static class DocumentParser
+    {
+        /// <summary> Получить коллекцию значений "value" с расчётным значением локации значения и пути в json-документе </summary>
+        /// <param name="json">json document</param>
+        /// <returns>Список кортежей (Значение, Локация, JPath)</returns>
+        public static List<(string Value, string Location, string JPath)> GetDocumentValuesWithLocations(string json)
+        {
+            return GetValues(json).Select(value => (value.ToString().Trim(), CalcLocationFromJPath(value.Path), value.Path)).ToList();
+        }
+
+        /// <summary>
+        /// Расчёт местоположение величины "value" в структуре json-документа
+        /// </summary>
+        /// <param name="path">JPath-путь</param>
+        private static string CalcLocationFromJPath(string path)
+        {
+            var stringBuilder = new StringBuilder();
+            var matches       = Regex.Matches(path, @"node(\[(?<idx>\d+)\])+").ToArray();
+
+            var mIdx = 0;
+            foreach (var m in matches)
+            {
+                ++mIdx;
+
+                var captures = m.Groups["idx"].Captures.ToArray();
+
+                if (mIdx == 1 && captures.Length > 0)
+                {
+                    stringBuilder.Append("P" + captures[0].Value);
+                    continue;
+                }
+
+                var cIdx = 0;
+                foreach (var c in captures)
+                {
+                    ++cIdx;
+                    stringBuilder.Append("-");
+                    stringBuilder.Append(mIdx == matches.Length && cIdx == captures.Length ? "P" : "D");
+                    stringBuilder.Append(c.Value);
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+
+
+        /// <summary>
+        /// Получить список объектоа со свойством "value" после разбивки каждого сложного объекта на множество простых <seealso cref="SplitValues"/>
+        /// </summary>
+        /// <param name="inputJson">json-документ</param>
+        /// <returns>массив JToken содержащих значение "value" и путь в дереве json-документа, из которого будет расчитано свойство Location</returns>
+        private static IEnumerable<JToken> GetValues(string inputJson)
+        {
+            var jToken            = JToken.Parse(inputJson);
+            var unseparatedValues = jToken.SelectTokens("$..value");
+            SplitValues(unseparatedValues);
+            return jToken.SelectTokens("$..value");
+        }
+
+        /// <summary>
+        /// Объект со свойством "value" может содержать значение со списком элементов
+        /// Например объект со значением [ ... { "value": " TOF(AI_RAW_TMP.LTMROFF,?,?) TON(AI_RAW_TMP.LTMRON,?,?)"} ... ]
+        /// эквивалентен тому, как если бы в массиве элементов было бы два объекта: [ ... { "value": " TOF(AI_RAW_TMP.LTMROFF,?,?)"}, { "value": " TON(AI_RAW_TMP.LTMRON,?,?)"	} ... ]
+        /// - Разбиваем такие объекты на множество простых.
+        /// </summary>
+        private static void SplitValues(IEnumerable<JToken> values)
+        {
+            foreach (var jToken in values.ToArray())
+            {
+                var value = jToken.Value<string>();
+
+                var matches = Regex.Matches(value, @"\w+\(.*?\)").Reverse().ToArray();
+                if (matches.Length <= 1) continue;
+
+                var jObjectWithValue = jToken.Parent.Parent;
+                    
+                foreach (var m in matches)
+                {
+                    var newToken = JObject.FromObject(new { value = m.Value });
+                    jObjectWithValue.AddAfterSelf(newToken);
+                }
+
+                jObjectWithValue.Remove();
+            }
+        }
+    }
+}
--- JsonReaderApp/Program.cs
+++ JsonReaderApp/Program.cs
@@ -2,75 +2,136 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
     using System.IO;
+    using System.Linq;
 
-    using Newtonsoft.Json;
-    using Newtonsoft.Json.Linq;
-
-    /// <summary> Конревой элемент json-документа </summary>
-    public class Root
-    {
-        public long Id { get; set; }
-        public NodeCollection Nodes { get; set; }
-    }
-    
-    /// <summary> Коллекция разнотипных элементов. Элементом коллекции может быть сама коллекция такого же типа <see cref="NodeCollection"/>,
-    /// объект со свойством "node" <see cref="Node"/> и объект, со свойством "value" <see cref="NodeValue"/>.
-    /// </summary>
-    public class NodeCollection: List<ICollectionItem>, ICollectionItem { }
-
-    /// <summary> Элемент коллекции - общий знаменатель для элементов коллекции NodeCollection </summary>
-    public interface ICollectionItem { }
-
-    /// <summary> Объект в json-документе, содержащий свойство "node" - коллекцию разнотипных элементов <see cref="NodeCollection"/> </summary>
-    public class Node: ICollectionItem
-    {
-        [JsonProperty("node")]
-        public NodeCollection Nodes { get; set; }
-    }
-    
-    /// <summary> Объект в json-документе со строковым свойством "value" </summary>
-    public class NodeValue : ICollectionItem
-    {
-        [JsonProperty("value")] 
-        public string Value { get; set; }
-    }
-    
-    /// <summary> Конвертер сериализатора для типа ICollectionItem </summary>
-    public class CollectionItemConverter : JsonConverter<ICollectionItem>
-    {
-        /// <inheritdoc/>
-        public override ICollectionItem ReadJson(JsonReader reader, Type objectType, ICollectionItem existingValue, bool hasExistingValue, JsonSerializer serializer)
-        {
-            var jToken = JToken.Load(reader);
-            ICollectionItem target;
-            if (jToken is JArray) target = new NodeCollection();
-            else if (jToken["node"] != null) target = new Node();
-            else if (jToken["value"] != null) target = new NodeValue();
-            else
-                throw new Exception("Неизвестный тип объекта " + jToken);
-
-            serializer.Populate(jToken.CreateReader(), target);
-            return target;
-        }
-
-        /// <inheritdoc/>
-        public override void WriteJson(JsonWriter writer, ICollectionItem value, JsonSerializer serializer) => serializer.Serialize(writer, value);
-    }
-    
     class Program
     {
         static void Main()
         {
-            var inputJson = File.ReadAllText("input.json");
-            var root = JsonConvert.DeserializeObject<Root>(inputJson, new CollectionItemConverter());
+            var errorCounter = 0;
+            const int testCount = 6;
 
-            var outputJson = JsonConvert.SerializeObject(root, Formatting.None);
-            var minimizedInputJson = JToken.ReadFrom(new JsonTextReader(new StringReader(inputJson))).ToString(Formatting.None);
+            for (var testNumber = 1; testNumber <= testCount; testNumber++)
+            {
+                var (json, testData) = GetTestData(testNumber);
 
-            Console.WriteLine(outputJson);
-            Debug.Assert(minimizedInputJson == outputJson);
+                WriteLine(ConsoleColor.White, $"\nTest #{testNumber}\n");
+                
+                var values = DocumentParser.GetDocumentValuesWithLocations(json);
+            
+                if (values.Count != testData.Count)
+                {
+                    errorCounter ++;
+                    WriteLine(ConsoleColor.DarkRed, $"Test data records count: {testData.Count}. Parsed values count: {values.Count()}");
+                }
+
+                Console.WriteLine($"{"    JPath", -44} - {"Expected value",18} - {"Calculated value", 18}");
+                Console.WriteLine("--------------------------------------------------------------------------------------");
+
+                foreach (var (value, location, jPath) in values)
+                {
+                    var expectedLocation = testData.FirstOrDefault(x => x.Value.Trim().Equals(value)).Location;
+
+                    if (expectedLocation != location)
+                    {
+                        errorCounter++;
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                    }
+                    
+                    Console.WriteLine($"{jPath, -44} - {expectedLocation,18} - {location, 18}");
+                    Console.ResetColor();
+                }
+            }
+
+            if (errorCounter == 0)
+                WriteLine(ConsoleColor.Green, $"\nПройдено {testCount} тестов. Количество ошибок: 0. Задача решена", ConsoleColor.Yellow, " (либо множество тестов было не полным).");
+            else
+                WriteLine(ConsoleColor.DarkRed, "Задача не решена. Количество ошибок: " + errorCounter);
+        }
+
+        /// <summary>
+        /// Тестовые данные
+        /// </summary>
+        /// <param name="testNumber">Номер тестового Json-документа</param>
+        static (string Json, List<(string Value, string Location)> valuesWithLocaltion) GetTestData(int testNumber)
+        {
+            if (testNumber < 1 || testNumber > 6)
+            {
+                throw new ArgumentOutOfRangeException(nameof(testNumber), testNumber, "value must be in 1..6");
+            }
+
+            var testData = new [] {
+                new List<(string Value, string Location)>
+                {
+                    ( "XIO(_HS_600B.State)"                                                                                             , "P0-D0-P0"  ),
+                    ( "XIO(_HS_600A.State)"                                                                                             , "P0-D1-P0"  ),
+                    ( "XIO(_ESD_PB_5.State)"                                                                                            , "P0-D2-P0"  ),
+                    ( "XIO(_ESD_PB_4.State)"                                                                                            , "P0-D3-P0"  ),
+                    ( "XIO(_ESD_PB_1.State)"                                                                                            , "P0-D4-P0"  ),
+                    ( "XIO(_ESD_PB_2.State)"                                                                                            , "P0-D5-P0"  ),
+                    ( "XIO(_HS_2099.State)"                                                                                             , "P0-D6-P0"  ),
+                    ( "XIO(_ESD_PB_3.State)"                                                                                            , "P0-D7-P0"  ),
+                    ( "ALMD(_HS_600B_Alm,_HS_600B_Alm.ProgAck,_HS_600B_Alm.ProgReset,_HS_600B_Alm.ProgDisable,_HS_600B_Alm.ProgEnable)" , "P1"        ),
+                },
+                new List<(string Value, string Location)>
+                {
+                    ( "GRT(AI_RAW_TMP.PV,AI_RAW_TMP.LSP)"  , "P0-D0-P0"  ),
+                    ( "XIO(AI_RAW_TMP.LEN)"                , "P0-D1-P0"  ),
+                    ( "TOF(AI_RAW_TMP.LTMROFF,?,?)"        , "P1"        ),
+                    ( "TON(AI_RAW_TMP.LTMRON,?,?)"         , "P2"        ),
+                },
+                new List<(string Value, string Location)>
+                {
+                    ( "LEQ(F20[32],40.0)"     , "P0"        ),
+                    ( "XIC(B50[23].8)"        , "P1-D0-P0"  ),
+                    ( "LEQ(F20[32],35.0)"     , "P1-D1-P0"  ),
+                    ( "TOF(AIT_TMR[21],?,?)"  , "P2"        ),
+                },
+                new List<(string Value, string Location)>
+                {
+                    ( "XIC(N100[348].12)"  , "P0-D0-P0"     ),
+                    ( "XIC(T42[48].TT)	"  , "P0-D1-P0"     ),
+                    ( "XIO(N100[348].13)"  , "P1"           ),
+                    ( "TON(T42[48],?,?)"   , "P2-D0-P0"     ),
+                    ( "OTE(N100[348].14)"  , "P2-D1-P0"     ),
+                },
+                new List<(string Value, string Location)>
+                {
+                    ( "XIC(B3[0].0)"          , "P0"        ),
+                    ( "XIC(B3[2].3)"          , "P1"        ),
+                    ( "XIC(_LSHH_221.State)"  , "P2"        ),
+                    ( "XIC(_XY_200)"          , "P3-D0-P0"  ),
+                    ( "XIC(B3[10].14)"        , "P3-D1-P0"  ),
+                    ( "OTE(_XY_200)"          , "P4"        ),
+                },
+                new List<(string Value, string Location)>
+                {
+                    ( "AnalogIn(PIT_502,Rack_6:1:I.Ch5Data,PIT_502_ALMA.HHLimit,PIT_502_ALMA.HLimit,PIT_502_ALMA.LLimit,PIT_502_ALMA.LLLimit,Rack_6:1:I.Ch5Fault,B3[10].14,PIT_502_string,Rack_6_1_Running)"
+                                                                                                                  , "P0"  ),
+                    ( "XIC(PIT_502.FLT)"                                                                          , "P1-D0-P0"  ),
+                    ( "OTE(PIT_502_ALMA.InFault)"                                                                 , "P1-D0-P1"  ),
+                    ( "ALMA(PIT_502_ALMA,PIT_502.PV,B3[10].14,PIT_502_ALMA.ProgDisable,PIT_502_ALMA.ProgEnable)"  , "P1-D1-P0"  ),
+                },
+            };
+                
+            return (File.ReadAllText($"test_{testNumber}.json"), testData[testNumber-1]);
+        }
+
+        private static void Write(params object[] outputs)
+        {
+            foreach (var output in outputs)
+            {
+                if (output is ConsoleColor color) Console.ForegroundColor = color;
+                else Console.Write(output);
+            }
+            Console.ResetColor();
+        }
+
+        private static void WriteLine(params object[] outputs)
+        {
+            Write(outputs);
+            Console.WriteLine();
         }
     }
 }
